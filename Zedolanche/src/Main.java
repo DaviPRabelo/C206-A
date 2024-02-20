8722b26f5920f8958727356722bfc66a2fde01ad
DaviPRabelo <160641658+DaviPRabelo@users.noreply.github.com>
2024-02-20 14:39:23 -0300

    Add files via upload

---
+++ Zedolanche/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int lanche1 = 10;
+        int lanche2 = 4;
+        int lanche3 = 2;
+
+        int lanchetotal = lanche1 + lanche2 + lanche3;
+        float media = lanchetotal/3.0f;
+
+        System.out.println("O total de lanches foi: "+lanchetotal);
+        System.out.println("A media de lanches foi: "+media);
+    }
+
+}
